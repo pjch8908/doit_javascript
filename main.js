8ef8581125ba58623fa7e9f22eb413162f6bb3a5
Goback9 <75929653+pjch8908@users.noreply.github.com>
2021-04-01 23:08:02 +0900

    doit JS : 14-1 age-result

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const heading = document.querySelector("#heading");
+/*const heading = document.querySelector("#heading");
 
 heading.onclick = function () {
   heading.style.color = "red";
@@ -12,3 +12,13 @@ const name1 = prompt("이름을 입력하세요.");
 console.log(name1);
 
 // document.write(`${name}님 안녕하세요`);
+*/
+
+const currentYear = 2021;
+let birthYear;
+let age;
+
+birthYear = prompt("태어난 연도를 입력하세요.(yyyy)", "");
+age = currentYear - birthYear + 1;
+document.write(`${currentYear}년 현재 `);
+document.write(`${birthYear}년에 태어난 사람의 나이는 ${age}세 입니다`);
