2a2e3f075d8fccff26720330f71c9c580ac39f2e
Goback9 <75929653+pjch8908@users.noreply.github.com>
2021-04-01 23:23:19 +0900

    doit JS : Math.round

--- main.js
+++ main.js
@@ -14,11 +14,24 @@ console.log(name1);
 // document.write(`${name}님 안녕하세요`);
 */
 
+/* 나이 계산기
 const currentYear = 2021;
 let birthYear;
 let age;
+
 
 birthYear = prompt("태어난 연도를 입력하세요.(yyyy)", "");
 age = currentYear - birthYear + 1;
 document.write(`${currentYear}년 현재 `);
 document.write(`${birthYear}년에 태어난 사람의 나이는 ${age}세 입니다`);
+*/
+
+/*실수 에서 버그 수정 / Math.round 사용후 12번째에서 반올림
+https://zorba91.tistory.com/266
+
+
+let a = 0.1;
+let b = 0.2;
+let usingRound = Math.round((a + b) * 1e12) / 1e12;
+console.log(usingRound);
+*/
