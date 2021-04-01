4a5e52d287481a4201412f0cb967f6f8e15a3044
Goback9 <75929653+pjch8908@users.noreply.github.com>
2021-04-01 22:52:39 +0900

    doit JS : 13-4

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const heading = document.querySelector("#heading");
 
 heading.onclick = function () {
-  heading.style.color = "blue";
+  heading.style.color = "red";
 };
+
+alert("안녕하세요");
+
+confirm("정말 배경 이미지를 바꾸겠습니까?");
+
+const name1 = prompt("이름을 입력하세요.");
+console.log(name1);
+
+// document.write(`${name}님 안녕하세요`);
