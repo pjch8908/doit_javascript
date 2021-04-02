35f7bd6141d3fda4aec38826fe66efd433652217
Goback9 <75929653+pjch8908@users.noreply.github.com>
2021-04-02 12:13:55 +0900

    doit JS : 14-4 if~else

--- main.js
+++ main.js
@@ -35,3 +35,44 @@ let b = 0.2;
 let usingRound = Math.round((a + b) * 1e12) / 1e12;
 console.log(usingRound);
 */
+
+/* 3의 배수 확인하기 1
+
+let userNumber = prompt("숫자를 입력하세요");
+
+if (userNumber % 3 === 0) {
+  alert("3의 배수입니다.");
+} else {
+  alert("3의 배수가 아닙니다.");
+}
+*/
+
+/* 3의 배수 확인하기 2
+
+let userNumber = prompt("숫자를 입력하세요.");
+
+if (userNumber !== null) {
+  if (userNumber % 3 === 0) {
+    alert("3의 배수입니다.");
+  } else {
+    alert("3의 배수가 아닙니다.");
+  }
+} else {
+  alert("입력이 취소되었습니다");
+}
+*/
+
+/* 조건 연산자로 조건 체크하기
+
+// (조건) ? true일 때 실행할 명령 : false 일때 실행할 명령
+
+let userNumber = prompt("숫자를 입력하세요.");
+
+if (userNumber !== null) {
+  userNumber % 3 === 0
+    ? alert("3의 배수입니다.")
+    : alert("3의 배수가 아닙니다.");
+} else {
+  alert("입력이 취소 되었습니다.");
+}
+*/
